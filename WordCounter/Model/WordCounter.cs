14c6e8e779e8e039ec46f6f6de65919d8d0f4a9e
Lina Shadrach <lina.shadrach@gmail.com>
2018-11-27 13:38:08 -0800

    change tests with lina

--- WordCounter/Model/WordCounter.cs
+++ WordCounter/Model/WordCounter.cs
@@ -13,48 +13,42 @@ namespace WordCounter
       _capturedWord = word;
       _capturedSentence = sentence;
     }
-    // public string ReturnString()
-    // {
-    //   string compareString = _capturedWord;
-    //   return compareString;
-    // }
-    // public string ReturnSentence()
-    // {
-    //   string compareSentence = _capturedSentence;
-    //   return compareSentence;
-    // }
-    // public string[] SplitSentence()
-    // {
-    //   string userSentence = _capturedSentence;
-    //   string[] splitArr = userSentence.Split(' ');
-    //   return splitArr;
-    // }
-    // public string LetterCounter()
-    // {
-    //   string wordToBeCounted = _capturedWord;
-    //   string sentenceToBeSplit = _capturedSentence;
-    //   string[] splitArr = sentenceToBeSplit.Split(' ');
-    //   int counter = 0;
-    //   for (int i = 0; i < splitArr.Length; i++)
-    //   if (splitArr[i] == wordToBeCounted)
-    //   {
-    //     counter++;
-    //   }
-    //   string counterCount = counter.ToString();
-    //   return counterCount;
-    // }
-    public int WordCounter()
+    public string ReturnString()
     {
-      string wordToBeCounted = _capturedWord.ToLower();
-      string sentenceToBeSplit = _capturedSentence.ToLower();
-      char[] splitRules = {' ',',','.','!','?'};
-      string[] splitArr = sentenceToBeSplit.Split(splitRules);
+      string compareString = _capturedWord.ToLower();;
+      return compareString;
+    }
+    public string ReturnSentence()
+    {
+      string compareSentence = _capturedSentence.ToLower();
+      return compareSentence;
+    }
+    public string[] SplitSentence(char[] splitRules)
+    {
+      string userSentence = _capturedSentence;
+      string[] splitArr = userSentence.Split(splitRules);
+      return splitArr;
+    }
+    public string LetterCounter(string wordToBeCounted, string sentenceToBeSplit)
+    {
+      string[] splitArr = sentenceToBeSplit.Split(' ');
       int counter = 0;
       for (int i = 0; i < splitArr.Length; i++)
-      if (splitArr[i] == wordToBeCounted)
       {
-        counter++;
+        if (splitArr[i] == wordToBeCounted)
+        {
+          counter++;
+        }
       }
+      return counter;
+    }
+    public int WordCounter()
+    {
+      string wordToBeCounted = ReturnString();
+      string sentenceToBeSplit = ReturnSentence();
+      char[] splitRules = {' ',',','.','!','?'};
+      string[] splitArr = SplitSentence(splitRules);
+      int counter = LetterCounter(wordToBeCounted, sentenceToBeSplit);
       return counter;
     }
     // public string ToLowerCase()
